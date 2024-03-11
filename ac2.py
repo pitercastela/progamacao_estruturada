4ff851f819afb195a38d5a97f8ae0cd47fb03121
Lucas Alves <lucasacastela@gmail.com>
2024-03-10 21:28:29 -0300

    commit

--- ac2.py
+++ ac2.py
@@ -6,14 +6,14 @@ def bhaskara(a, b, c):
     raiz2 = (-b - delta)/(a * 2)
     return raiz1, raiz2
 
-print(bhaskara(1, -6, 8))
-
-#só pra criar uma separação entre os exercícios
-print("-" * 60)
+def ano_bissexto(ano):
+    resultado = str((ano % 100 == 0) and (ano % 400 == 0) or (ano % 100 != 0) and (ano % 4 == 0))
+    return resultado
 
 #exercício 2
 
-def ano_bissexto(ano = 2000):
-    resultado = str((ano % 100 == 0) and (ano % 400 == 0) or (ano % 100 != 0) and (ano % 4 == 0))
-    return resultado
-print(ano_bissexto())
+def calcula_salario(valor_hora, num_horas, irpf = 0.275):
+    salario_bruto = valor_hora * num_horas
+    salario_liquido = salario_bruto - salario_bruto * irpf
+    return salario_liquido
+

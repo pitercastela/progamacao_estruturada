c2bef3cb4828695d6accaf168aa895b3009ddf27
Lucas Alves <lucasacastela@gmail.com>
2024-03-19 10:09:01 -0300

    adicionado ac5

---
+++ ac5.py
@@ -0,0 +1,32 @@
+import random
+
+def main():
+    #aventureiro
+    hp_aventureiro = 100
+    atk_aventureiro = random.randint(10, 20)
+    defesa_aventureiro =random.randint(1, 5)
+
+    #monstro
+    hp_monstro = random.randint(60, 80)
+    atk_monstro = random.randint(20, 30)
+
+    rodada = 1
+
+    print("aventureiro: vida", hp_aventureiro, "-", "att", atk_aventureiro, "-", "def", defesa_aventureiro)
+    print("monstro: vida", hp_monstro, "-", "att", atk_monstro)
+
+    while rodada:
+        print("rodada", rodada)
+        hp_monstro = hp_monstro - random.randint(1, atk_aventureiro)
+        if hp_monstro <= 0:
+            print("O monstro morreu!")
+            break
+        hp_aventureiro = hp_aventureiro - (random.randint(1, atk_monstro) - defesa_aventureiro)
+        rodada += 1
+        if hp_aventureiro<= 0:
+            print("O aventureiro morreu!")
+            break
+        print("aventureiro: vida", hp_aventureiro, "-", "att", atk_aventureiro, "-", "def", defesa_aventureiro)
+        print("monstro: vida", hp_monstro, "-", "att", atk_monstro)
+
+main()

4f75a557afb55a94c20a2336333dca851a9a9b1a
Lucas Alves <lucasacastela@gmail.com>
2024-04-16 20:31:28 -0300

    ac7

--- ac7.py
+++ ac7.py
@@ -1,5 +1,4 @@
 #1048
-'''
 salario = float(input())
 if salario >= 0 and salario <= 400.00:
     reajuste = 0.15
@@ -16,9 +15,9 @@ novo_salario = salario + salario*reajuste
 print(f"Novo salario: {novo_salario:.2f}")
 print(f"Reajuste ganho: {salario*reajuste:.2f}")
 print("Em percentual:", int(reajuste*100),"%")
-'''
+
 #1065
-'''
+
 numeros = []
 numerospares = 0
 for i in range(5):
@@ -28,14 +27,66 @@ for i in numeros:
     if i % 2 == 0:
         numerospares += 1
 print(numerospares, "valores pares")
-'''
+
 #1132
-numeros = []
+numerais = []
 for i in range(2):
     numeros.append(int(input()))
-numeros = sorted(numeros)
+numeraiss = sorted(numeros)
 resultado = 0
-for i in range(numeros[0], numeros[1]+1):
+for i in range(numerais[0], numerais[1]+1):
     if i % 13 != 0:
         resultado += i
 print(resultado)
+
+#1773
+numero = int(input())
+n = []
+for i in range(10):
+    n.append(numero)
+    numero *= 2
+    print(f"N[{i}] = {n[i]}")
+
+#1180
+
+qt_de_vezes = int(input())
+lista_junta = input()
+lista_strings = lista_junta.split()
+lista_int = []
+for i in range(qt_de_vezes):
+    lista_int.append(int(lista_strings[i]))
+lista_sorteada = sorted(lista_int)
+menor_numero = lista_sorteada[0]
+print(f"Menor valor: {menor_numero}")
+for i in range(qt_de_vezes):
+    if lista_int[i] == menor_numero:
+        print(f"Posicao: {i}")
+
+#1882
+coluna = int(input())
+operacao = input()
+matriz = []
+resultado = 0
+for i in range(12):
+    linha = []
+    for i in range(12):
+        linha.append(float(input()))
+    matriz.append(linha)
+
+if operacao == "s":
+    for i in range(12):
+        resultado += matriz[coluna][i]
+elif operacao == "m":
+    for i in range(12):
+        resultado += matriz[coluna][i]
+    resultado = resultado/12
+print(resultado)
+
+#1244
+n = int(input())
+for _ in range(n):
+    entrada = input()
+    palavras = entrada.split(" ")
+
+    palavras.sort(key=len, reverse=True)
+    print(" ".join(palavras))

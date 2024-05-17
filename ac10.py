5799883654570533f5e413676e35ec6235331ede
LucasACastela <85808717+pitercastela@users.noreply.github.com>
2024-05-17 17:40:25 -0300

    Add files via upload

--- ac10.py
+++ ac10.py
@@ -1,18 +1,20 @@
 #arvores
 a = int(input())
-for i in range(a):
-    arvores = []
-    qtd = []
-    while True:
-        b = input()
-        if b == "":
-            break
-        arvores.append(b)
-    arvores.sort()
-    arvores_sem =list(dict.fromkeys(arvores))
-    qtd_total = len(arvores)
-    for i in arvores_sem:
-        c = arvores.count(i)
-        qtd.append(c)
-    for i in range(len(arvores_sem)):
-        print(f"{arvores_sem[i]} {(qtd[i]/qtd_total)*100:.4f}")
+if input() == "":
+    for i in range(a):
+        arvores = []
+        qtd = []
+        while True:
+            b = input()
+            if b == "":
+                break
+            arvores.append(b)
+        arvores.sort()
+        arvores_sem =list(dict.fromkeys(arvores))
+        qtd_total = len(arvores)
+        for i in arvores_sem:
+            c = arvores.count(i)
+            qtd.append(c)
+        for i in range(len(arvores_sem)):
+            print(f"{arvores_sem[i]} {(qtd[i]/qtd_total)*100:.4f}")
+        print()
